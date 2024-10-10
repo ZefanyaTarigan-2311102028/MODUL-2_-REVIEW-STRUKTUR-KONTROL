1cf94f1c93bc999734b9854e0f701f5672265d9a
ZefanyaTarigan-2311102028 <zefanyabrana@gmail.com>
2024-10-11 00:11:16 +0700

    Add files via upload

---
+++ 2C/3.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("bilangan: ")
+	fmt.Scanln(&n)
+	fmt.Print("Faktor: ")
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			fmt.Print(i, " ")
+		}
+	}
+	fmt.Println()
+	var jumlahFaktor int = 0
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			jumlahFaktor++
+		}
+	}
+	if jumlahFaktor == 2 {
+		fmt.Println("Prima: true")
+	} else {
+		fmt.Println("Prima: false")
+	}
+}
